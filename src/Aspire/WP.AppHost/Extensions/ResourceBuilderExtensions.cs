43d357b1e4d9b63a491c94a391340ade7c7efeb2
Henrique S. Domareski <hsd.foz@gmail.com>
2025-04-14 21:01:44 +0200

    Upgraded projects to .NET 9 and added OpenAPI UIs (#2)
    * Upgraded projects to .NET 9 and added OpenAPI UIs
    
    * Integrated OpenAPI UIs with Aspire
    
    * Update ResourceBuilderExtensions.cs
    
    * Fixed typo

---
+++ src/Aspire/WP.AppHost/Extensions/ResourceBuilderExtensions.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Diagnostics;
+
+namespace WP.AppHost.Extensions
+{
+    internal static class ResourceBuilderExtensions
+    {
+        internal static IResourceBuilder<T> WithSwaggerUI<T>(this IResourceBuilder<T> builder)
+            where T: IResourceWithEndpoints
+        {
+            return builder.WithOpenApiDocs("swagger-ui-docs", "Swagger API Documentation", "swagger");
+        }
+
+        internal static IResourceBuilder<T> WithScalar<T>(this IResourceBuilder<T> builder)
+            where T : IResourceWithEndpoints
+        {
+            return builder.WithOpenApiDocs("scalar-docs", "Scalar API Documentation", "scalar/v1");
+        }
+
+        internal static IResourceBuilder<T> WithReDoc<T>(this IResourceBuilder<T> builder)
+            where T : IResourceWithEndpoints
+        {
+            return builder.WithOpenApiDocs("redoc-docs", "ReDoc API Documentation", "api-docs");
+        }
+
+        private static IResourceBuilder<T> WithOpenApiDocs<T>(
+            this IResourceBuilder<T> builder,
+            string name,
+            string displayName,
+            string openApiUiPath)
+            where T : IResourceWithEndpoints
+        {
+            return builder.WithCommand(
+                name,
+                displayName,
+                context =>
+                {
+                    try
+                    {
+                        // Base URL
+                        var endpoint = builder.GetEndpoint("https");
+                        var url = $"{endpoint.Url}/{openApiUiPath}";
+
+                        Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+
+                        return Task.FromResult(new ExecuteCommandResult { Success = true });
+                    }
+                    catch (Exception ex)
+                    {
+                        return Task.FromResult(new ExecuteCommandResult { Success = false, ErrorMessage = ex.ToString() });
+                    }
+                },
+                new CommandOptions
+                {
+                    UpdateState = context => context.ResourceSnapshot.HealthStatus == HealthStatus.Healthy
+                        ? ResourceCommandState.Enabled
+                        : ResourceCommandState.Disabled,
+                    IconName = "Document",
+                    IconVariant = IconVariant.Filled
+                });
+        }
+    }
+}
---
+++ src/WP.Identity.API/Configuration/OpenApiConfig.cs
@@ -0,0 +1,34 @@
+﻿using Scalar.AspNetCore;
+
+namespace WP.Identity.API.Configuration
+{
+    public static class OpenApiConfig
+    {
+        public static void AddOpenApiConfiguration(this IServiceCollection services)
+        {
+            services.AddOpenApi();
+        }
+
+        public static void UseOpenApiConfiguration(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                // Swagger
+                app.MapOpenApi();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/openapi/v1.json", "WP Identity API");
+                });
+
+                // ReDoc
+                app.UseReDoc(options =>
+                {
+                    options.SpecUrl("/openapi/v1.json");
+                });
+
+                // Scalar
+                app.MapScalarApiReference();
+            }
+        }
+    }
+}
