91138a3f3c519efb07eeb85f3116275b68d3245b
henriquesd <hsd.foz@gmail.com>
2025-12-21 20:46:24 +0100

    Implemented WebAPI.Core

---
+++ src/WP.WebAPI.Core/User/IAspNetUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace WP.WebAPI.Core.User
+{
+    public interface IAspNetUser
+    {
+        string Name { get; }
+        Guid GetUserId();
+        string GetUserEmail();
+        string GetUserToken();
+        string GetUserRefreshToken();
+        bool IsAuthenticated();
+        bool HasRole(string role);
+        IEnumerable<Claim> GetClaims();
+        HttpContext GetHttpContext();
+    }
+}
